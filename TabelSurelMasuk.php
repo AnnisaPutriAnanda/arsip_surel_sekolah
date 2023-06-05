6bef8a64981308dc6ad3866acc7c6c59c2f5497b
AnnisaPutriAnanda <annisaputripku01@gmail.com>
2023-06-05 17:08:24 +0700

    first commit

---
+++ TabelSurelMasuk.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Arsip Surel Sekolah</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="navigation_bar">
+
+    <nav class="navbar navbar-expand-sm bg-success navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+    <li class="nav-item">
+        <a class="navbar-brand" href="#">
+          <img src="LOGO_SMK_LABOR_HD.png" alt="Logo" style="width:80px;" class="rounded-pill">
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="perpustakaan.php">Perpustakaan Digital</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kontak Sekolah</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
+
+  <div class="container-fluid overflow-auto">
+
+ <div class="bg-image" style="background-image: url('https://scontent.fpku4-1.fna.fbcdn.net/v/t39.30808-6/294391448_548486346900647_8273495434617047595_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=e3f864&_nc_ohc=sOzrp8lISiEAX8nzWPp&_nc_ht=scontent.fpku4-1.fna&oh=00_AfB8PByRn-yBHshHiZHaXmD3YQC2inkbmckv7sDpaiWDiA&oe=648211C2'); height: 400px;">
+  <img src="LOGO_SMK_LABOR_HD.png"  style="width:150px;">
+
+  <div class="text-center" style=""><h1 class="text-light">SMK LABOR PEKANBARU</h1></div>
+
+</div>
+
+  <h1 class="text-center">Mengenai<h1>
+  <p class="text-sm-center" style="font-size: 18px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
+  
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ perpustakaan.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Arsip Surel Sekolah</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="navigation_bar">
+
+    <nav class="navbar navbar-expand-sm bg-success navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+    <li class="nav-item">
+        <a class="navbar-brand" href="#">
+          <img src="LOGO_SMK_LABOR_HD.png" alt="Logo" style="width:80px;" class="rounded-pill">
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="TabelSurelMasuk.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Perpustakaan Digital</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kontak Sekolah</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
+
+  <!-- Carousel -->
+  <div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+<!-- Indicators/dots -->
+<div class="carousel-indicators">
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+</div>
+
+<!-- The slideshow/carousel -->
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img src="perpus.jpg" alt="Los Angeles" class="d-block" style="width:100%">
+    <div class="carousel-caption">
+      <h3>Hang Tuah</h3>
+    </div>
+  </div>
+  <div class="carousel-item">
+    <img src="perpus2.jpg" alt="Chicago" class="d-block" style="width:100%">
+    <div class="carousel-caption">
+      <h3>Hang Kasturi</h3>
+    </div> 
+  </div>
+  <div class="carousel-item">
+    <img src="perpus.jpg" alt="New York" class="d-block" style="width:100%">
+    <div class="carousel-caption">
+      <h3>Hang Tuah</h3>
+    </div>  
+  </div>
+</div>
+
+<!-- Left and right controls/icons -->
+<button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+  <span class="carousel-control-next-icon"></span>
+</button>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
